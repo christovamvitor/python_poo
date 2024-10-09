00f0effe6cda52f869ea11ea049f0af75496fa1e
christovam_vitor <christovamovitor@gmail.com>
2024-10-09 10:13:29 -0300

    Atualização código

--- luta/main.py
+++ luta/main.py
@@ -3,12 +3,19 @@ from lutadores import *
 popo = Boxeador("Popó")
 bambam = Boxeador("Bambam")
 
+charles = MMA("Charles do Bronx")
+khabib = MMA("Khabib")
+
 print(popo)
 print(bambam)
-
 popo.cruzado(bambam)
 print(bambam)
 popo.gancho(bambam)
 print(bambam)
 bambam.soco(popo)
 print(popo)
+
+print(charles)
+print(khabib)
+charles.superman_punch(khabib)
+print(khabib)
